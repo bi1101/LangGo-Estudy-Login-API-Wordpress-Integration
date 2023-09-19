fa07eb656c12c7186b3c80b74ade2f5c4351c757
bi1101 <bihungba1101@GMAIL.COM>
2023-09-19 11:09:19 +0700

    handle user pass change on main site

--- function.php
+++ function.php
@@ -1,44 +1,73 @@
 <?php
 add_filter('authenticate', 'langgo_authenticate', 30, 3);
 
-function langgo_authenticate($user, $username, $password) {
+function langgo_authenticate($user, $username, $password)
+{
     // If previous authentication succeeded, then return the user.
-    if (is_a($user, 'WP_User')) { 
-        return $user; 
+    if (is_a($user, 'WP_User')) {
+        return $user;
     }
 
     // If WP user does not exist or authentication failed, check against your API.
     $response = call_langgo_login_api($username, $password);
-    
-    if ($response->status) {
-        $random_email = wp_generate_password(12, false) . "@example.com"; 
-        $user_id = wp_create_user($username, $password, $random_email);
 
-        // Optionally update the user's display name with the name from the API.
-        wp_update_user([
-            'ID' => $user_id,
-            'display_name' => $response->user_infor->name,
-			'nickname' => $response->user_infor->name
-        ]);
+    // Successful authentication.
+    if ($response->status) {
+        $wp_user = get_user_by('login', $username);
+
+        // If user already exists in WP, update their details.
+        if ($wp_user) {
+            $userdata = array(
+                'ID' => $wp_user->ID,
+                'user_pass' => $password,
+                'display_name' => $response->user_infor->name,
+                'nickname' => $response->user_infor->name
+            );
+
+            $user_id = wp_update_user($userdata);
+
+        } else {
+            // If user doesn't exist, create a new WP user.
+            $random_email = wp_generate_password(12, false) . "@example.com";
+            $user_id = wp_create_user($username, $password, $random_email);
+            wp_update_user([
+                'ID' => $user_id,
+                'display_name' => $response->user_infor->name,
+                'nickname' => $response->user_infor->name
+            ]);
+        }
 
         // Return the user for successful login.
         $user = new WP_User($user_id);
         return $user;
-    }
+    } else {
+        // Handle specific API error messages.
+        if (isset($response->errors)) {
+            $errors = (array) $response->errors;
+            $error_messages = [];
+            foreach ($errors as $field => $messages) {
+                $error_messages[] = implode('; ', $messages);
+            }
 
-    // Handle different API error messages.
-    return new WP_Error('invalid_login', __($response->message));
+            return new WP_Error('api_error', implode('; ', $error_messages));
+        }
+        return new WP_Error('api_error', __($response->message));
+    }
 }
 
-function call_langgo_login_api($username, $password) {
+function call_langgo_login_api($username, $password)
+{
     $api_endpoint = "https://api-estudy.langgo.vn/api/v1/writify/login";
 
-    $response = wp_remote_post($api_endpoint, array(
-        'body' => array(
-            'username' => $username,
-            'password' => $password
+    $response = wp_remote_post(
+        $api_endpoint,
+        array(
+            'body' => array(
+                'username' => $username,
+                'password' => $password
+            )
         )
-    ));
+    );
 
     $body = wp_remote_retrieve_body($response);
 
